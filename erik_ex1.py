be7804a12897d5aa9ed76b4a38e9bca5e34e2816
Erik Barnett <ebarnett@uber.com>
2017-03-20 14:15:50 -0700

    Add Phython Exercise One

---
+++ erik_ex1.py
@@ -0,0 +1,10 @@
+#!/usrbin/env python
+
+my_str1 = "John Doe"
+my_str2 = "Jane Doe"
+my_str3 = "Erik Barnett"
+my_str4 = raw_input("Enter fourth name: ")
+
+print "{:>30}{:>30}{:>30}".format(my_str1, my_str2, my_str3)
+
+print my_str4
