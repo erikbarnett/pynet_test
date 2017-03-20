95abe3dae627a7c9916d1f3446257639a037993e
Erik Barnett <ebarnett@uber.com>
2017-03-20 14:38:30 -0700

    Python tasks 1 and 2 updates

--- erik_ex1.py
+++ erik_ex1.py
@@ -1,10 +1,9 @@
-#!/usrbin/env python
 
-my_str1 = "John Doe"
+my_str1 = "John Smith"
 my_str2 = "Jane Doe"
 my_str3 = "Erik Barnett"
-my_str4 = raw_input("Enter fourth name: ")
 
 print "{:>30}{:>30}{:>30}".format(my_str1, my_str2, my_str3)
 
+my_str4 = raw_input("Enter fourth name: ")
 print my_str4
